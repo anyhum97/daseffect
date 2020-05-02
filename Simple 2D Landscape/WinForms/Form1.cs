2dbf5b655cce147fb58105f6fc7dfec775692df5
anyhum97 <anyhum@yandex.ru>
2020-05-02 16:14:49 +0300

    Save Image

--- Simple 2D Landscape/WinForms/Form1.cs
+++ Simple 2D Landscape/WinForms/Form1.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
 using System.Windows.Forms;
 using Simple_2D_Landscape.LandscapeEngine;
 
@@ -128,31 +129,64 @@ namespace Simple_2D_Landscape
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                try
+                {
+                    if(!Directory.Exists("Pictures"))
+                        Directory.CreateDirectory("Pictures");
+
+                    pictureBox1.Image.Save("Pictures\\" + 
+                                           DateTime.Now.ToString("dd") + "d" + 
+                                           DateTime.Now.ToString("MM") + "m" + 
+                                           DateTime.Now.ToString("yy") + "y and " + 
+                                           
+                                           DateTime.Now.ToString("hh") + "h" + 
+                                           DateTime.Now.ToString("mm") + "m" + 
+                                           DateTime.Now.ToString("ss") + "s.png");
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!Directory.Exists("Pictures"))
+                        Directory.CreateDirectory("Pictures");
+
+            Process.Start(Directory.GetCurrentDirectory() + "\\Pictures");
+        }
+
         //private void openGLControl1_OpenGLDraw_1(object sender, RenderEventArgs args)
         //{
         //    // Create a Simple Sample:
 
         //    OpenGL gl = openGLControl1.OpenGL;
-            
+
         //    // Clear Screen & Depth Buffer
         //    gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-            
+
         //    // Reset World Matrix
         //    gl.LoadIdentity();
-            
+
         //    // Move Draw Pointer to -Z coords.
         //    gl.Translate(0.0f, 0.0f, -5.0f);
-            
+
         //    // Begin Drawing
         //    gl.Begin(OpenGL.GL_TRIANGLES);
-            
+
         //    // Use White Color
         //    gl.Color(1.0f, 1.0f, 1.0f);
-            
+
         //    gl.Vertex(-1.0f, -1.0f);
         //    gl.Vertex(0.0f, 1.0f);
         //    gl.Vertex(1.0f, -1.0f);
-            
+
         //    // Draw triangle with points(vertex) { { -1.0f, -1.0f }, { 0.0f, 1.0f }, { 1.0f, -1.0f } }
 
         //    // Stop Drawing
