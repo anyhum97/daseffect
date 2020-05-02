7d336051e22f8d7224b2e8a7fc9c068e9897313f
anyhum97 <anyhum@yandex.ru>
2020-05-02 15:04:26 +0300

    using fix

--- Simple 2D Landscape/WinForms/Form1.cs
+++ Simple 2D Landscape/WinForms/Form1.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using SharpGL;
 using Simple_2D_Landscape.LandscapeEngine;
 
 namespace Simple_2D_Landscape
