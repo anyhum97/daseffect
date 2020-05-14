dcc38ad9388c5e92d999961e92db3fcd937f9494
anyhum97 <anyhum@yandex.ru>
2020-05-15 00:54:58 +0300

    OpenTK Triangle

--- User Interface/MainWindow.xaml.cs
+++ User Interface/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK.Graphics.ES20;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Utils;
 
 namespace User_Interface
 {
@@ -41,8 +43,6 @@ namespace User_Interface
 
 			daseffect.Set(1, 128, 128, 1.0f);
 
-			MainImage.Source = ImageToByte(daseffect.GetBitmap());
-
 			Timer timer = new Timer();
 
 			timer.Interval = 100;
@@ -50,10 +50,100 @@ namespace User_Interface
 			timer.Start();
 		}
 
+         private bool canDraw = false;
+        private int program;
+        private int nVertices;
+
 		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
 		{
 			daseffect.Iteration();
 			//MainImage.Source = ImageToByte(daseffect.GetBitmap());
 		}
+
+		private void WindowsFormsHost_Initialized(object sender, EventArgs e)
+        {
+            //renderCanvas.MakeCurrent();
+        }
+
+        private void renderCanvas_Load(object sender, EventArgs e)
+        {
+            // Load shaders from files
+
+            string vShaderSource = null;
+            string fShaderSource = null;
+
+            ShaderLoader.LoadShader("Shaders/VertexShader.glsl", out vShaderSource);
+            ShaderLoader.LoadShader("Shaders/FragmentShader.glsl", out fShaderSource);
+
+            if (vShaderSource == null || fShaderSource == null)
+            {
+                return;
+            }
+
+            // Initialize the shaders
+            if (!ShaderLoader.InitShaders(vShaderSource, fShaderSource, out program))
+            {
+                return;
+            }
+
+            // Write the positions of vertices to a vertex shader
+            nVertices = InitVertexBuffers();
+            if (nVertices <= 0)
+            {
+                return;
+            }
+
+            // Specify the color for clearing
+            GL.ClearColor(System.Drawing.Color.DarkSlateBlue);
+
+            canDraw = true;
+        }
+
+        private int InitVertexBuffers()
+        {
+            float[] vertices = new float[] { 0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
+
+            // Create a buffer object
+            int vertexBuffer;
+            GL.GenBuffers(1, out vertexBuffer);
+
+            // Bind the buffer object to target
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+
+            // Write data into the buffer object
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            // Get the storage location of a_Position
+            int a_Position = GL.GetAttribLocation(program, "a_Position");
+            if (a_Position < 0)
+            {
+                return -1;
+            }
+
+            // Assign the buffer object to a_Position variable
+            GL.VertexAttribPointer(a_Position, 2, VertexAttribPointerType.Float, false, 0, 0);
+
+            // Enable the assignment to a_Position variable
+            GL.EnableVertexAttribArray(a_Position);
+
+            return vertices.Length / 2;
+        }
+
+        private void renderCanvas_Paint(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, renderCanvas.Width, renderCanvas.Height);
+
+            // Clear the render canvas with the current color
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            if (canDraw)
+            {
+                // Draw a triangle
+                GL.DrawArrays(PrimitiveType.Triangles, 0, nVertices);
+            }
+
+            GL.Flush();
+            renderCanvas.SwapBuffers();
+        }
 	}
 }
